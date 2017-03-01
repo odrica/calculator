123e0bd1d0f4f313b9e0cff8adc75a0977e59b8e
OdoricaAjency <odorica@ajency.in>
2017-03-01 16:23:36 +0530

    task 7

--- calculator.php
+++ calculator.php
@@ -1,27 +1,18 @@
 <?php
+
 $sum = 0;
-$string = false;
-foreach($argv as $key => $val)
-{ 
-    if(is_numeric($val))
-      $sum += $val;
-    else if(is_string($val) && $key > 1){
-      $delimiter = substr($val, 2,1);
-      $value = substr($val, 5,strlen($val));
-      $array = explode($delimiter, $value);
-      foreach ($array as $vals) {
-        if($vals < 0){
-          $string .= $vals.", ";
-        }
-        if(is_numeric($vals))
-          $sum += $vals;
+foreach($argv as $key => $value)
+{
+    if($key > 0 && is_numeric($value))
+      $sum += $value;
+    else if(is_string($value)){
+      $array = explode(',', $value);
+      foreach ($array as $k => $val) {
+        if($val < 1000)
+        $sum += $val;
       }
     }
 
-}
-if($string){
-  echo "Error: Negative  numbers (".rtrim($string, ", ").") not allowed."."\n";
-  exit();
 }
 
 echo $sum."\n";
