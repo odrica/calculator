8cc8aa46b91ddd305e8434ce16fef92ad479a474
OdoricaAjency <odorica@ajency.in>
2017-03-01 16:52:04 +0530

    task 8

--- calculator.php
+++ calculator.php
@@ -1,19 +1,25 @@
 <?php
-
-$sum = 0;
+$multi = 1;
 foreach($argv as $key => $value)
 {
-    if($key > 0 && is_numeric($value))
-      $sum += $value;
-    else if(is_string($value)){
+    if($key  == 1 && $value != 'multiply'){
+      echo "Command '".$value. "' incorrect!!"."\n";
+      exit();
+    }
+    if($key > 1 && is_numeric($value))
+      $multi *= $value;
+    else if(is_string($value) && $key > 1){
       $array = explode(',', $value);
       foreach ($array as $k => $val) {
-        if($val < 1000)
-        $sum += $val;
+        if($val < 1000 && is_numeric($val))
+          $multi *= $val;
       }
     }
 
 }
 
-echo $sum."\n";
+if(!isset($argv[2]))
+  $multi = 0;
+
+echo $multi."\n";
 ?>
